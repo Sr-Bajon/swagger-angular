7513d381935d0c31a99d12d5cd7d763d206379fc
Ismael Rodríguez González <ismael.rodriguez@mad.atmira.org>
2016-11-29 13:49:13 +0100

    Parseo de la definicion swagger a javascript completado

--- generator.js
+++ generator.js
@@ -1,8 +1,6 @@
 const sw = require('swagger-parser');
-// const YAML     = require('yamljs');
 const fs = require('fs');
 const _  = require('lodash');
-// const chokidar = require('chokidar');
 
 const parser = new sw();
 
@@ -11,56 +9,79 @@ const conf = require('./config.json');
 separator();
 parseSwaggerSpec();
 
+function parseSwaggerSpec() {
+  parser.validate(conf.swaggerFileSource)
+    .then(() => {
+      console.log('Deferencing');
+      return parser.dereference(conf.swaggerFileSource);
+    })
+    .then(api => {
+      getApiConfig(api);
+
+      return api;
+    })
+    .then(api => {
+      getPaths(api.paths);
+    })
+    .catch(err => {
+      separator();
+      console.log('Ha ocurrido un error');
+      separator();
+      throw err;
+    });
+}
+
 function separator() {
   console.log(new Array(100).join("="));
 }
 
-function parseSwaggerSpec() {
-  parser.validate(conf.swaggerFileSource)
-    .then(function (resolve) {
-      console.log('Deferencing');
-      return parser.dereference(conf.swaggerFileSource);
-    })
-    .then(function (api) {
-      getApiConfig(api);
+function getPaths(paths) {
+  normalizeKeys(paths);
+  reorganize(paths);
+  purgue(paths, conf.purgeList);
 
-      let paths = api.paths;
-      normalizeKeys(paths);
-      reorganize(paths);
+  separator();
+  console.log('Escribiendo archivo apiConf.json');
+  fs.writeFileSync(conf.apiPathgDest, JSON.stringify(paths, null, 2));
+}
 
-      separator();
-      console.log('Escribiendo archivo apiConf.json');
-      fs.writeFileSync(conf.apiPathgDest, JSON.stringify(paths, null, 2));
+function isObj(obj) {
+  return Object.prototype.toString.call(obj) === "[object Object]";
+}
 
-
-    })
-    .catch((err) => {
-      separator();
-      console.log('Ha ocurrido un error');
-      separator();
-      console.log(err);
-      separator();
-    });
+function purgue(obj, purgueList) {
+  Object.keys(obj).forEach(key => {
+    if (purgueList.indexOf(key) >= 0) {
+      delete obj[key];
+    } else if (isObj(obj[key])) {
+      purgue(obj[key], purgueList);
+    }
+  });
 }
 
 function reorganize(obj) {
   Object.keys(obj).forEach(key => {
-    // "stores.order.orderId"
-    let keySplit = key.split('.');
-    // ['stores', 'order', 'orderId']
-    let newObject = {};
-    let objectIterator;
+    let newObj = {
+      objIterator: {}
+    };
+    let auxObj = newObj.objIterator;
+    key.split('.').forEach(keyPartial => {
+      if (!auxObj[keyPartial]) auxObj[keyPartial] = {};
+      auxObj = auxObj[keyPartial];
+    });
 
+    newObj = newObj.objIterator;
+    _.assign(auxObj, obj[key]);
+    delete obj[key];
+    _.merge(obj, newObj);
   });
 }
-
 
 function normalizeKeys(obj) {
   Object.keys(obj).forEach(key => {
     let newKey  = key
       .slice(1)
       .replace(/\//g, '.')
-      // .replace('/', '.')
       .replace('{', '')
       .replace('}', '');
     obj[newKey] = {};
@@ -68,7 +89,6 @@ function normalizeKeys(obj) {
     delete obj[key];
   })
 }
-
 
 function getApiConfig(api) {
   let apiConf = {};
