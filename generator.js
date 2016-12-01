e5eddd65b718bf96bdc589abae58124014afc577
Ismael Rodríguez González <ismael.rodriguez@mad.atmira.org>
2016-12-01 16:37:48 +0100

    Desarrollo

--- generator.js
+++ generator.js
@@ -20,7 +20,7 @@ function parseSwaggerSpec() {
       return api;
     })
     .then(api => {
-      getPaths(api.paths);
+      getPaths(api);
     })
     .catch(err => {
       console.log('Ha ocurrido un error');
@@ -28,20 +28,28 @@ function parseSwaggerSpec() {
     });
 }
 
-function getPaths(paths) {
-  setUrlMethod(paths);
-  normalizeKeys(paths);
-  reorganize(paths);
-  purgue(paths, conf.purgeList);
+function getPaths(api) {
+  setUrl(api.paths);
+  setMethod(api.paths);
+  setConsumes(api);
+  mergeParameters(api.paths);
+  normalizeKeys(api.paths);
+  reorganize(api.paths);
+  purgue(api.paths, conf.purgeList);
 
   console.log('Escribiendo archivo apiPath.json');
-  fs.writeFileSync(conf.apiPathgDest, JSON.stringify(paths, null, 2));
+  fs.writeFileSync(conf.apiPathgDest, JSON.stringify(api.paths, null, 2));
 }
 
 function isObj(obj) {
   return Object.prototype.toString.call(obj) === "[object Object]";
 }
 
+/**
+ * Elimina los datos innecesarios de la especificacion swagger
+ * @param obj
+ * @param purgueList
+ */
 function purgue(obj, purgueList) {
   Object.keys(obj).forEach(key => {
     if (purgueList.indexOf(key) >= 0) {
@@ -52,6 +60,10 @@ function purgue(obj, purgueList) {
   });
 }
 
+/**
+ * Convierte todos los paths en objetos de forma que se pueda acceder a ellos con dot notation
+ * @param obj
+ */
 function reorganize(obj) {
   Object.keys(obj).forEach(key => {
     let newObj = {
@@ -72,16 +84,83 @@ function reorganize(obj) {
   });
 }
 
-function setUrlMethod(obj) {
+/**
+ * Si hay parametros generales en el path se incluyen en cada metodo respetando los especificos del metodo
+ * @param obj
+ */
+function mergeParameters(obj) {
+  Object.keys(obj).forEach((key) => {
+    if (obj[key].parameters) {
+      // si el path tiene parametros generales lo mergeo con cada metodo
+      Object.keys(obj[key]).forEach(method => {
+        if (method !== 'parameters') {
+          if (!obj[key][method].parameters) {
+            // si el method no tiene parametros y hay un general lo copio tal cual
+            obj[key][method].parameters = obj[key].parameters.slice();
+          } else {
+            // hay un parameters general y uno en el method, copio los parametros generales que no tenga y mantengo
+            // los especificos duplicados
+            obj[key].parameters.forEach(generalParam => {
+              let encontrado = false;
+              obj[key][method].parameters.forEach(methodParam => {
+                if (methodParam.name === generalParam.name) encontrado = true;
+              });
+              if (!encontrado) obj[key][method].parameters.push(generalParam);
+            });
+          }
+          // por ultimo borro parameters del path pues ya no lo necesito
+          delete obj[key].parameters;
+        }
+      });
+    }
+  });
+}
+
+/**
+ * Crea una clave url con la url a la que hay que llamar en cada metodo
+ * @param obj
+ */
+function setUrl(obj) {
   let urls = Object.keys(obj);
   Object.keys(obj).forEach((key, index) => {
     Object.keys(obj[key]).forEach(method => {
-      obj[key][method].url    = urls[index].replace(/\/{.*}$/, '');
+      obj[key][method].url = urls[index].replace(/\/{.*}$/, '');
+    });
+  });
+}
+
+/**
+ * Crea una clave method para saber cual es el metodo del endpoint en cada metodo
+ * @param obj
+ */
+function setMethod(obj) {
+  Object.keys(obj).forEach((key) => {
+    Object.keys(obj[key]).forEach(method => {
       obj[key][method].method = method.toUpperCase();
     });
   });
 }
 
+/**
+ * Si hay un consumes general lo incluye en cada metodo a no ser que el metodo ya tenga un consumes propio
+ * @param obj
+ */
+function setConsumes(obj) {
+  debugger;
+  if (obj.consumes) {
+    Object.keys(obj.paths).forEach((path) => {
+      Object.keys(obj.paths[path]).forEach((method) => {
+        if (!obj.paths[path][method].consumes) obj.paths[path][method].consumes = obj.consumes.slice();
+      });
+
+    });
+  }
+}
+
+/**
+ * cambia la clave de los paths de /ruta/subruta/{rutaId} a ruta.subruta.rutaId
+ * @param obj
+ */
 function normalizeKeys(obj) {
   Object.keys(obj).forEach(key => {
     let newKey  = key
@@ -95,6 +174,10 @@ function normalizeKeys(obj) {
   })
 }
 
+/**
+ * Obtiene determinados valores generales de la especificacion swagger
+ * @param api
+ */
 function getApiConfig(api) {
   let apiConf = {};
   if (api.host) apiConf.host = api.host;
